1ed26c019acc281d6046e481625f37fc20e29114
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-11-24 00:29:59 +0200

    template firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOt5sD8GrEkW6o2tIrAYjD7Hh4eEW4U8I",
+  authDomain: "movie-5e2cf.firebaseapp.com",
+  projectId: "movie-5e2cf",
+  storageBucket: "movie-5e2cf.appspot.com",
+  messagingSenderId: "816660778695",
+  appId: "1:816660778695:web:af417d0ccf90b5bebc38e2",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../firebase-config";
+import {
+  User,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState<User | null>(null);
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label>
+          Email
+          <input
+            type="email"
+            name="email"
+            onChange={(event) => {
+              setLoginEmail(event.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            onChange={(event) => {
+              setLoginPassword(event.target.value);
+            }}
+          />
+        </label>
+        <button type="button" onClick={login}>
+          Log In
+        </button>
+        <p>or login with social media</p>
+        <ul>
+          <li>Google</li>
+          <li>Facebook</li>
+          <li>GitHub</li>
+        </ul>
+        <button type="button" onClick={logout}>
+          Sign out {user?.email}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
