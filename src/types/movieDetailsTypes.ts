24d4ab45f8da1f19b76da8e3a684ed41230bacd3
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2023-11-26 19:30:27 +0100

    correct_types

--- src/types/movieDetailsTypes.ts
+++ src/types/movieDetailsTypes.ts
@@ -1,12 +1,12 @@
 export interface IMovieDetails {
-  id: number;
-  title: string;
-  poster_path: string;
-  vote_average: number;
-  release_date: string;
-  genres: {
+  id?: number;
+  title?: string;
+  poster_path?: string;
+  vote_average?: number;
+  release_date?: string;
+  genres?: {
     id: number;
     name: string;
   }[];
-  overview: string;
+  overview?: string;
 }
