ddb72ba6e9e080fe674075fbf8e41871d5e1af3f
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-12-03 22:46:33 +0200

    added nested routes for settings and pages; added import img in ts; added UserModal;

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,15 +1,24 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+import UserModal from '../UserModal/UserModal';
 //  Коли користувач успішно пройшов реестрацію або логін
 const UserMenu = () => {
   const { t } = useTranslation();
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
   return (
     <>
       <NavLink to="/favorites">{t('layout.favorite')}</NavLink>
       <div>
         {/* дінамічне ім'я користувача */}
-        <NavLink to='/settings'>{t('layout.username')}</NavLink>
+        <button type="button" onClick={handleShow}>
+          {t('layout.username')}
+        </button>
+        <UserModal close={handleClose} show={show} />
         <button>{t('layout.logout')}</button>
       </div>
     </>
