efc08fb91fb4657a677abdd912c70ae5a94a0ef4
ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>
2023-12-05 23:25:40 +0200

    Update index.tsx
    Signed-off-by: ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ import Text from "./Language/utils/i18n";
 import { I18nextProvider } from "react-i18next";
 import { LanguageProvider } from "./components/Language/LanguageContext";
 import { ToastContainer } from "react-toastify";
-import {  UserProvider } from "./components/UserContext/UserContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -19,9 +18,7 @@ root.render(
     <BrowserRouter>
       <LanguageProvider>
         <I18nextProvider i18n={Text}>
-            <UserProvider>
             <App />
-           </UserProvider>
         </I18nextProvider>
         <ToastContainer />
       </LanguageProvider>
