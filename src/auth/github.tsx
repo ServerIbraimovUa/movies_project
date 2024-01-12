d14620ff9640b9dbce0b036f191e1f1880bdd937
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-01-12 22:13:25 +0200

    google+facebook+github

---
+++ src/auth/github.tsx
@@ -0,0 +1,38 @@
+import { signInWithPopup } from "firebase/auth";
+import { GitHubProvider, auth } from "../firebase-config";
+import { writeUserData } from "../db/writeData";
+import { UserType } from "../types/user";
+import { useUser } from "../context/UserContext";
+
+export const SignInWithGithub = () => {
+  const { logIn } = useUser()!;
+
+  const handleSignIn = async () => {
+    try {
+      const github = await signInWithPopup(auth, GitHubProvider);
+
+      if (github.user) {
+        const username = github.user.displayName || "";
+        logIn({ username, uid: github.user.uid });
+
+        const newUser = {
+          uid: github.user.uid,
+          username,
+          language: "ua",
+          theme: "light" as const,
+        } as UserType;
+
+        // записуємо нового юзера в базу даних
+        writeUserData(newUser);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <button type="button" onClick={handleSignIn}>
+      Github
+    </button>
+  );
+};
