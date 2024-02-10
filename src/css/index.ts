37cc0b1ba0905e944fae5665bef8e3e1b892c5b3
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-02-10 10:37:59 +0200

    create css

---
+++ src/css/index.ts
@@ -0,0 +1,146 @@
+import { createGlobalStyle } from 'styled-components';
+import WorkSansRegular from '../assets/fonts/WorkSans/WorkSans-Regular.ttf';
+import WorkSansMedium from '../assets/fonts/WorkSans/WorkSans-Medium.ttf';
+import WorkSansSemiBold from '../assets/fonts/WorkSans/WorkSans-SemiBold.ttf';
+import ZenMaruRegular from '../assets/fonts/ZenMaruGothic/ZenMaruGothic-Regular.ttf';
+import ZenMaruMedium from '../assets/fonts/ZenMaruGothic/ZenMaruGothic-Medium.ttf';
+import ZenMaruBold from '../assets/fonts/ZenMaruGothic/ZenMaruGothic-Bold.ttf';
+import { device } from './deviceSize';
+import '../';
+
+export const GlobalStyle = createGlobalStyle`
+
+:root{
+    // COLORS
+    /* LIGHT THEME */
+    --dark-violet-clr: #9040F6;
+    --violet-clr: #ECDEFF;
+    --light-violet-clr: #F4EBFF;
+    --bg-clr-light-theme: rgba(113, 0, 255, 0.05);
+
+    --text-clr-black: #2F2E30;
+    --text-clr-grey: #61646B;
+    --text-clr-white: #FBF8FF;
+    --text-clr-red: #f85d52;
+
+    --violet-disabled-clr: #CFC5DC;
+
+    /* DARK THEME */
+    --dark-clr: #1C1C1C;
+    --grey-bg-clr: #61646B;
+    --grey-disabled-clr: #E4E4E4;
+
+    /* HOVER */
+    --violet-hover: #6d0fe4;
+    --grey-hover: #c7c7c7;
+
+
+/* SHADOW */
+// DARK
+    --shadow: box-shadow: 2px 2px 10px 0 rgba(0, 0, 0, 0.15);
+
+    // FONTS
+    --font-size: 20px;
+    --line-height: 1.2;
+    --regular-font-weight: 400;
+    --medium-font-weight: 500;
+    --semi-bold-font-weight: 600;
+    --bold-font-weight: 700;
+    
+}
+
+
+/* FONTS */
+
+@font-face {
+    font-family: 'WorkSans-Regular',sans-serif;
+    src: url(${WorkSansRegular}) format('truetype'),
+    }
+    @font-face {
+    font-family: 'WorkSans-Medium',sans-serif;
+    src: url(${WorkSansMedium}) format('truetype'),
+    }
+    @font-face {
+    font-family: 'WorkSans-SemiBold',sans-serif;
+    src: url(${WorkSansSemiBold}) format('truetype'),
+    }
+    @font-face {
+    font-family: 'ZenMaruGothic-Regular',sans-serif;
+    src: url(${ZenMaruRegular}) format('truetype'),
+    }
+     @font-face {
+    font-family: 'ZenMaruGothic-Medium',sans-serif;
+    src: url(${ZenMaruMedium}) format('truetype'),
+    }
+     @font-face {
+    font-family: 'ZenMaruGothic-Bold',sans-serif;
+    src: url(${ZenMaruBold}) format('truetype'),
+    }
+    
+
+/* STANDART STYLES */
+    
+    body {
+    overflow-x: hidden;
+    font-family: 'ZenMaru-Gothic', 'WorkSans', sans-serif;
+    font-weight: var(--regular-font-weight);
+    font-style: normal;
+    font-size: var(--font-size);
+    line-height: var(--line-height);
+    
+    color: var(--text-clr-black);
+    background-color: var(--bg-clr-light-theme);   
+}
+
+ .is-hidden {
+  opacity: 0;
+  visibility: hidden;
+  pointer-events: none;
+}
+
+.no-scroll {
+  overflow: hidden;
+}
+
+.visually-hidden {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
+  -webkit-clip-path: inset(100%);
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
+
+.container {
+  width: 100%;
+  margin: 0 auto; 
+  
+  @media ${device.mobile} {
+        max-width: 320px;
+        }
+  
+  @media ${device.tablet} {
+        max-width: 744px;
+        }
+
+    @media ${device.desktop} {        
+        max-width: 1264px;       
+      }
+}
+
+section{
+  padding-top:68px;     
+
+  @media ${device.tablet} {
+    padding-top:  155px;     
+  }
+
+  @media ${device.desktop} {
+    padding-top: 150px;
+  }
+}`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
-
-import Text from "./Language/utils/i18n";
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import { GlobalStyle } from './css';
+import { BrowserRouter } from 'react-router-dom';
+import Text from './Language/utils/i18n';
 import { ThemeProvider } from './components/SwitcherTheme/ThemeContext';
-
-import { I18nextProvider } from "react-i18next";
-import { LanguageProvider } from "./components/Language/LanguageContext";
-import { ToastContainer } from "react-toastify";
-import { UserProvider } from "./context/UserContext";
+import { I18nextProvider } from 'react-i18next';
+import { LanguageProvider } from './components/Language/LanguageContext';
+import { ToastContainer } from 'react-toastify';
+import { UserProvider } from './context/UserContext';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
@@ -24,6 +21,7 @@ root.render(
         <I18nextProvider i18n={Text}>
           <ThemeProvider>
             <UserProvider>
+              <GlobalStyle />
               <App />
             </UserProvider>
           </ThemeProvider>
