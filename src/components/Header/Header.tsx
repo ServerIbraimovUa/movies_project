4d836e1b7fc660c39595615f3dbe6e9b6ca96acd
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-03-01 20:20:54 +0200

    fix setting mobile modal appearance

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
-import { FC, useState } from "react";
-import icons from "../../assets/images/sprite.svg";
+import { FC, useState } from 'react';
+import icons from '../../assets/images/sprite.svg';
 
-import LanguageSelector from "../Language/LanguageSelector";
-import SwitcherTheme from "../SwitcherTheme/SwitcherTheme";
-import UserMenu from "../UserMenu/UserMenu";
+import LanguageSelector from '../Language/LanguageSelector';
+import SwitcherTheme from '../SwitcherTheme/SwitcherTheme';
+import UserMenu from '../UserMenu/UserMenu';
 import {
   ContainerHeader,
   NavBar,
@@ -11,26 +11,27 @@ import {
   SettingMobileModalBtn,
   SwitcherContainerDesk,
   SwitcherContainerMob,
-} from "./Header.styled";
-import SettingsMobModal from "../SettingsMobModal/SettingsMobModal";
-import { useUser } from "../../context/UserContext";
-import Icon from "../Icon/Icon";
-import { NavLink } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
-import SearchMovies from "../SearchMovies/SearchMovies";
-import { Navbar } from "react-bootstrap";
-import AuthMenu from "../AuthMenu/AuthMenu";
+} from './Header.styled';
+import SettingsMobModal from '../SettingsMobModal/SettingsMobModal';
+import { useUser } from '../../context/UserContext';
+import Icon from '../Icon/Icon';
+import { NavLink } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import SearchMovies from '../SearchMovies/SearchMovies';
+import { Navbar } from 'react-bootstrap';
+import AuthMenu from '../AuthMenu/AuthMenu';
 
 const Header: FC = () => {
   const { isLoggedIn } = useUser()!;
   const [show, setShow] = useState(false);
+  const isMobile = useMediaQuery({ query: '(max-width: 1023px)' });
 
   const handleShow = () => {
     setShow(true);
   };
   const handleClose = () => setShow(false);
 
-  const isTabletOrDesktop = useMediaQuery({ query: "(min-width:1024px)" });
+  const isTabletOrDesktop = useMediaQuery({ query: '(min-width:1024px)' });
 
   return (
     <>
@@ -70,7 +71,7 @@ const Header: FC = () => {
           <LanguageSelector />
         </SwitcherContainerMob>
       </ContainerHeader>
-      <SettingsMobModal close={handleClose} show={show} />
+      {isMobile && <SettingsMobModal close={handleClose} show={show} />}
     </>
   );
 };
