d48b94100bd007b9a7f11150cb885fc72fa4af76
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2024-03-03 00:25:39 +0200

    comment footer import

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,16 @@
-import { FC } from "react";
-import { Outlet } from "react-router-dom";
-import Footer from "../Footer/Footer";
-import "./Layout.css";
-import { useTheme } from "../SwitcherTheme/ThemeContext";
-import { ThemeProvider } from "styled-components";
-import Header from "../Header/Header";
-
+import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
+// import Footer from "../Footer/Footer";
+import './Layout.css';
+import { useTheme } from '../SwitcherTheme/ThemeContext';
+import { ThemeProvider } from 'styled-components';
+import Header from '../Header/Header';
 
 const Layout: FC = () => {
   const { theme } = useTheme();
 
   return (
     <ThemeProvider theme={theme}>
-
       <header>
         <Header />
       </header>
@@ -20,7 +18,6 @@ const Layout: FC = () => {
         <Outlet />
       </main>
       <footer>{/* <Footer /> */}</footer>
-
     </ThemeProvider>
   );
 };
